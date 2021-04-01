50da5f2f796fabbf745ec4892b348e0c9abf78ec
Dmitriy Firsov <shtihlfd@yandex.ru>
2021-04-01 13:50:37 +0300

    Some refactor

--- module_b5_practice_1/tictactoe.py
+++ module_b5_practice_1/tictactoe.py
@@ -8,26 +8,20 @@ def game():
 
     show_field(play_field)
 
-    turn_count = 0
-
     for turn_count in range(10):
         if turn_count == 9:
             print("Ничья!")
             break
-        if turn_count % 2 == 0:
-            marker = " X "
-        else:
-            marker = " Y "
-
+        marker = " X " if turn_count % 2 == 0 else " Y "
+        
         coord_x, coord_y = users_input(play_field, marker)
         play_field[coord_x][coord_y] = marker
 
         if is_win(play_field, marker):
-            print(f"Игрок{marker} Выиграл!\nПоздравляем!")
-            show_field(play_field)
+            print(f"Игрок{marker}Выиграл!\nПоздравляем!")
             break
-        else:
-            show_field(play_field)
+
+        show_field(play_field)
 
 
 def show_field(field):
@@ -41,10 +35,12 @@ def users_input(field, marker):
         if len(place) != 2:
             print("Введите 2 координаты")
             continue
-        if not (place[0].isdigit() and place[1].isdigit()):
-            print("Введите числа")
+        try:
+            coord_x, coord_y = int(place[0]), int(place[1])
+        except ValueError:
+            print("Введите два числа")
             continue
-        coord_x, coord_y = map(int, place)
+
         if not (coord_x > 0 and coord_x < 4 and coord_y > 0 and coord_y < 4):
             print("Вышли за рамки поля")
             continue
@@ -61,12 +57,16 @@ def is_win(field, marker):
             return True
         return False
 
+    if (
+        check_line(field[1][1], field[2][2], field[3][3], marker)
+        or check_line(field[3][1], field[2][2], field[1][3], marker)
+    ):
+        return True
+
     for _ in range(1, 4):
         if (
             check_line(field[_][1], field[_][2], field[_][3], marker)
             or check_line(field[1][_], field[2][_], field[3][_], marker)
-            or check_line(field[1][1], field[2][2], field[3][3], marker)
-            or check_line(field[3][1], field[2][2], field[1][3], marker)
         ):
             return True
     return False
